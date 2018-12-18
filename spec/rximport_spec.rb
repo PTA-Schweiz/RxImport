bc6c8ad2961638f23f51de4f92eab80078f7b1f8
Stefan Staub <ste.staub@gmail.com>
2018-12-18 13:20:53 +0100

    bumped version

--- spec/rximport_spec.rb
+++ spec/rximport_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Rximport do
   it "has a version number" do
     expect(Rximport::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
